b77840191a5d9ff8f0af0be63f077c42b6cdedc1
Artem Ligerko <artmarket81@gmail.com>
2023-01-05 19:45:14 +0200

    feat:describe controllers

--- src/controllers/transactions.controller.js
+++ src/controllers/transactions.controller.js
@@ -4,48 +4,48 @@ import {
   getOne,
   updateOne,
   deleteOne,
-} from "./todo.service.js";
+} from "../services/transactions.services.js";
 
-export const createOneTodo = async (req, res) => {
+export const createOneTransaction = async (req, res) => {
   try {
-    const todo = await createOne(req.body);
-    return res.json(todo);
+    const response = await createOne(req.body);
+    return res.json(response);
   } catch (e) {
     res.status(500).json(e.message);
   }
 };
 
-export const getAllTodos = async (req, res) => {
+export const getAllTransactions = async (req, res) => {
   try {
-    const todos = await getAll();
-    return res.json(todos);
+    const response = await getAll();
+    return res.json(response);
   } catch (e) {
     res.status(500).json(e.message);
   }
 };
 
-export const getOneTodo = async (req, res) => {
+export const getOneTransaction = async (req, res) => {
   try {
-    const todo = await getOne(req.params.id);
-    return res.json(todo);
+    const response = await getOne(req.params.id);
+    return res.json(response);
   } catch (e) {
     res.status(500).json(e.message);
   }
 };
 
-export const updateOneTodo = async (req, res) => {
+export const updateOneTransaction = async (req, res) => {
   try {
-    const updatedTodo = await updateOne(req.body);
-    return res.json(updatedTodo);
+    const response = await updateOne(req.body);
+    return res.json(response);
   } catch (e) {
     res.status(500).json(e.message);
   }
 };
 
-export const deleteOneTodo = async (req, res) => {
+export const deleteOneTransaction = async (req, res) => {
   try {
-    const deletedTodo = await deleteOne(req.params.id);
-    return res.json(deletedTodo);
+    const response = await deleteOne(req.params.id);
+    return res.json(response);
   } catch (e) {
     res.status(500).json(e.message);
   }
